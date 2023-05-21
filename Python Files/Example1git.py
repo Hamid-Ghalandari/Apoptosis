88ecf673386145385a20dc3a05a022887c274ab5
Hamid <hmd_ghalandari@yahoo.com>
2023-05-21 21:12:00 +0330

    main file modified

--- Python Files/Example1git.py
+++ Python Files/Example1git.py
@@ -1,3 +1,14 @@
-a = 4
-if a == 5:
-    print("true")
+# a = 4
+# if a == 5:
+#     print("true")
+
+import csv
+
+CsvFile = open ('data.csv')
+CsvReader = csv.DictReader(CsvFile)
+
+lisOfDic = []
+ 
+for item in CsvReader:
+    lisOfDic.append(item)
+    print(item)
